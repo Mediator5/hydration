9570951e7f24dcfc9120d7711e0b2580d54170f3
Mediator5 <mdigital1196@gmail.com>
2026-01-09 08:22:11 +0100

    updating

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../build",
+    outDir: "build",
     emptyOutDir: true,
     rollupOptions: {
       input: {
