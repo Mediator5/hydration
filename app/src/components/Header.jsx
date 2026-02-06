92e8922e83dfc61858d09a0a7bf1f5cc5105cea8
Mediator5 <mdigital1196@gmail.com>
2026-02-06 08:59:54 +0100

    update

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -24,9 +24,10 @@ const menus = [
 export default function Header() {
     return (
         <header className="flex justify-between items-center px-5 py-3 bg-white shadow-md">
-            <div className="text-xl font-serif font-bold text-gray-700">
+            {/* <div className="text-xl font-serif font-bold text-gray-700">
                 Hydration Solution
-            </div>
+            </div> */}
+            <img className="w-96" src="https://caleblaw.io/wp-content/uploads/2026/02/Hydration-Solution-Logo-alternate2.png" alt="logo" />
             <nav className="flex space-x-6">
                 {
                     menus.map((menu) => (
@@ -35,7 +36,7 @@ export default function Header() {
                 }
 
             </nav>
-            
+
             <a href="#booking" className="bg-teal hover:bg-ocean text-white  py-3 px-6 rounded shadow-lg flex items-center transition duration-300 font-sans">
                 BOOK AN IV NOW →
             </a>
